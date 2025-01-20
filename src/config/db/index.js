fc47a242b72e378bca2ea16db899055ff976c80f
tuandaynek <103641348+tuandaynek@users.noreply.github.com>
2025-01-20 19:12:26 +0700

    Update index.js

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect(){
     try{
-        await mongoose.connect('mongodb+srv://TuanCodeNNLT:Tuanc06102023@test-database.vcoy8.mongodb.net/?retryWrites=true&w=majority&appName=Test-Database', {
+        await mongoose.connect('mongodb+srv://TuanCodeNNLT:Tuanc06102023@test-database.vcoy8.mongodb.net/testUpload?retryWrites=true&w=majority&appName=Test-Database', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
